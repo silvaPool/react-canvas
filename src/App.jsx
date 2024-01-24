d76421772d61742617410db5356e12db58392316
silvaPool <silva.friends22@gmail.com>
2024-01-24 13:57:50 -0300

    React-canvas

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import ReactCanvasPaint from 'react-canvas-paint'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+
+      <div>
+        <h1>ReactJs Drawing Canvas - Nirvana</h1>
+        <ReactCanvasPaint />
+      </div>
+  
+  
+    </>
+  )
+}
+
+export default App
